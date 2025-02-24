6e508d6ef85b0ec1b2a05da25d613a047cc5a804
MotejzikM <motejzikm@jirovcovka.net>
2025-02-24 15:49:51 +0100

    Delete obrazyapp directory

--- obrazyapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Obraz
-
-class ObrazForm(forms.ModelForm):
-    class Meta:
-        model = Obraz
-        fields = ["nazev", "autor", "rok", "technika", "cena", "poznamky", "img"]
